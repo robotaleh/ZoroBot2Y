79a801cb98de9de7ca5ca3e29964958663de3bc9
robotaleh <alex.santos.ete@gmail.com>
2017-10-15 12:34:33 +0200

    Añade la inicialización principal de todos los componentes

---
+++ src/Init.ino
@@ -0,0 +1,90 @@
+//////////////////////////////////////////////////////////////////
+// Este fichero contendrá todas las funciones que se            //
+// ejecutarán al inicio del programa, como inicializaciones de  //
+// componentes o del puerto Serial.                             //
+//////////////////////////////////////////////////////////////////
+
+
+/**
+* Inicializa todos los pines y componentes
+*/
+void init_all(){
+  init_serial();
+  init_motores();
+  init_encoders();
+  init_sensores();
+  init_btn_led();
+}
+
+/**
+ * Inicializa el puerto Serial
+ */
+void init_serial(){
+  Serial.begin(9600);
+  while(!Serial){}
+}
+
+/**
+* Inicializa los pines de los motores y los para.
+*/
+void init_motores(){
+  // Inicialización de pines
+  pinMode(MOTOR_DERECHO_ADELANTE, OUTPUT);
+  pinMode(MOTOR_DERECHO_ATRAS, OUTPUT);
+  pinMode(MOTOR_IZQUIERDO_ADELANTE, OUTPUT);
+  pinMode(MOTOR_IZQUIERDO_ATRAS, OUTPUT);
+  pinMode(MOTOR_DERECHO_PWM, OUTPUT);
+  pinMode(MOTOR_IZQUIERDO_PWM, OUTPUT);
+  pinMode(MOTOR_RUN, OUTPUT);
+
+  // Estado inicial; parado
+  digitalWrite(MOTOR_DERECHO_ADELANTE, LOW);
+  digitalWrite(MOTOR_DERECHO_ATRAS, LOW);
+  digitalWrite(MOTOR_IZQUIERDO_ADELANTE, LOW);
+  digitalWrite(MOTOR_IZQUIERDO_ATRAS, LOW);
+  digitalWrite(MOTOR_RUN, HIGH);
+  digitalWrite(MOTOR_DERECHO_PWM, LOW);
+  digitalWrite(MOTOR_IZQUIERDO_PWM, LOW);
+}
+
+/**
+* Inicializa los encoders en cuadratura
+* usando la librería "PinChangeInt.h" para
+* los pines fuera de interrupciones nativas
+* y obtiene sus estados iniciales.
+*/
+void init_encoders(){
+  // Inicializa los encoders.
+  attachInterrupt(digitalPinToInterrupt(ENCODER_DERECHO_B), tick_encoder_derecho_B, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_IZQUIERDO_B), tick_encoder_izquierdo_B, CHANGE);
+  attachPinChangeInterrupt(ENCODER_DERECHO_A, tick_encoder_derecho_A, CHANGE);
+  attachPinChangeInterrupt(ENCODER_IZQUIERDO_A, tick_encoder_izquierdo_A, CHANGE);
+
+  // Obtiene el estado de cada uno de los canales.
+  estado_encoder_derecho_A = !digitalRead(ENCODER_DERECHO_A);
+  estado_encoder_izquierdo_A = !digitalRead(ENCODER_IZQUIERDO_A);
+  estado_encoder_derecho_B = !digitalRead(ENCODER_DERECHO_B);
+  estado_encoder_izquierdo_B = !digitalRead(ENCODER_IZQUIERDO_B);
+}
+
+/**
+ * Inicializa todos los sensores
+ */
+void init_sensores(){
+  for(byte sensor = 0;sensor<NUM_SENSORES_FRONTALES;sensor++){
+    pinMode(sensores_frontales[sensor], INPUT);
+  }
+
+  for(byte sensor = 0;sensor<NUM_SENSORES_LATERALES;sensor++){
+    pinMode(sensores_laterales[sensor], INPUT);
+  }
+}
+
+/**
+ * Inicializa leds y botones
+ */
+void init_btn_led(){
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BTN_1, INPUT_PULLUP);
+  pinMode(BTN_2, INPUT_PULLUP);
+}
