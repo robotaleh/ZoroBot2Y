3c40f9404105b7334aa40efb29e724585efc31db
robotaleh <alex.santos.ete@gmail.com>
2017-10-15 16:15:47 +0200

    Añade calibración automática de laterales

--- src/Calibracion.ino
+++ src/Calibracion.ino
@@ -39,3 +39,63 @@ void calibrate_front(){
   stop(-80,-80);
 }
 
+/**
+* Realiza una calibración automática de los sensores laterales.
+*
+* NOTE: El robot debe de estar situado perpendicularmente a una pared frontal,
+*       con el eje de las ruedas en la línea imaginaria de los postes de la celda.
+*       Ej.: TODO: añadir link de foto de la wiki del apartado de calibración.
+*/
+void calibrate_sides(){
+  int sensor_values[NUM_SENSORES_FRONTALES];
+  float ticksMotores[2];
+  calcular_arco_giro(-15, ticksMotores);
+  ticksIzquierdo = 0;
+  ticksDerecho = 0;
+  short velBaseI = 50;
+  do {
+
+    read_sides_raw(sensor_values);
+    for(byte sensor=0; sensor<NUM_SENSORES_LATERALES; sensor++){
+      if (sensor_values[sensor] < minValoresLaterales[sensor]) {
+        minValoresLaterales[sensor] = sensor_values[sensor];
+      }
+      if (sensor_values[sensor] > maxValoresLaterales[sensor]) {
+        maxValoresLaterales[sensor] = sensor_values[sensor];
+      }
+    }
+
+    if (ticksIzquierdo >= ticksMotores[0] && ticksMotores[0]>0) {
+      if(velBaseI > 0){
+        velBaseI = -velBaseI;
+        ticksMotores[0] = 0;
+      }
+    }
+    set_speed(velBaseI, 0);
+  } while (velBaseI>0 || ticksIzquierdo > 0);
+  stop(-50,0);
+  ticksIzquierdo = 0;
+  ticksDerecho = 0;
+  short velBaseD = 50;
+  do {
+
+    read_sides_raw(sensor_values);
+    for(byte sensor=0; sensor<NUM_SENSORES_LATERALES; sensor++){
+      if (sensor_values[sensor] < minValoresLaterales[sensor]) {
+        minValoresLaterales[sensor] = sensor_values[sensor];
+      }
+      if (sensor_values[sensor] > maxValoresLaterales[sensor]) {
+        maxValoresLaterales[sensor] = sensor_values[sensor];
+      }
+    }
+
+    if (ticksDerecho >= ticksMotores[1] && ticksMotores[1]>0) {
+      if(velBaseD > 0){
+        velBaseD = -velBaseD;
+        ticksMotores[1] = 0;
+      }
+    }
+    set_speed(0, velBaseD);
+  } while (velBaseD > 0 || ticksDerecho > 0);
+  stop(0,-50);
+}
