e42d7e18fa4c4adebe8b9ddf1a6705f9ee62b1f4
robotaleh <alex.santos.ete@gmail.com>
2017-10-15 12:48:00 +0200

    Añade lógica de Cuadratura

--- src/Lecturas.ino
+++ src/Lecturas.ino
@@ -4,29 +4,81 @@
 ///////////////////////////////////////////////////////////////////
 
 /**
- * Función a ejecutar con cada tick del Canal A del motor derecho
- */
+* Función a ejecutar con cada tick del Canal A del motor derecho
+*/
 void tick_encoder_derecho_A(){
-
+  estado_encoder_derecho_A = !estado_encoder_derecho_A;
+  if(!estado_encoder_derecho_A){
+    if(estado_encoder_derecho_B){
+      ticksDerecho--;
+    }else{
+      ticksDerecho++;
+    }
+  }else{
+    if(!estado_encoder_derecho_B){
+      ticksDerecho--;
+    }else{
+      ticksDerecho++;
+    }
+  }
 }
 
 /**
- * Función a ejecutar con cada tick del Canal B del motor derecho
- */
+* Función a ejecutar con cada tick del Canal B del motor derecho
+*/
 void tick_encoder_derecho_B(){
-
+  estado_encoder_derecho_B = !estado_encoder_derecho_B;
+  if(!estado_encoder_derecho_B){
+    if(!estado_encoder_derecho_A){
+      ticksDerecho--;
+    }else{
+      ticksDerecho++;
+    }
+  }else{
+    if(estado_encoder_derecho_A){
+      ticksDerecho--;
+    }else{
+      ticksDerecho++;
+    }
+  }
 }
 
 /**
- * Función a ejecutar con cada tick del Canal A del motor izquierdo
- */
+* Función a ejecutar con cada tick del Canal A del motor izquierdo
+*/
 void tick_encoder_izquierdo_A(){
-
+  estado_encoder_izquierdo_A = !estado_encoder_izquierdo_A;
+  if(!estado_encoder_izquierdo_A){
+    if(estado_encoder_izquierdo_B){
+      ticksIzquierdo++;
+    }else{
+      ticksIzquierdo--;
+    }
+  }else{
+    if(!estado_encoder_izquierdo_B){
+      ticksIzquierdo++;
+    }else{
+      ticksIzquierdo--;
+    }
+  }
 }
 
 /**
- * Función a ejecutar con cada tick del Canal B del motor izquierdo
- */
+* Función a ejecutar con cada tick del Canal B del motor izquierdo
+*/
 void tick_encoder_izquierdo_B(){
-
+  estado_encoder_izquierdo_B = !estado_encoder_izquierdo_B;
+  if(!estado_encoder_izquierdo_B){
+    if(!estado_encoder_izquierdo_A){
+      ticksIzquierdo++;
+    }else{
+      ticksIzquierdo--;
+    }
+  }else{
+    if(estado_encoder_izquierdo_A){
+      ticksIzquierdo++;
+    }else{
+      ticksIzquierdo--;
+    }
+  }
 }
