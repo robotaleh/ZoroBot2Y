3aadf455fc63fac0cf41ac82084230e9c00f0044
robotaleh <alex.santos.ete@gmail.com>
2017-10-15 12:34:07 +0200

    Añade estructura para funciones de Encoders en Cuadratura

---
+++ src/Lecturas.ino
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////////////
+// Este fichero contendrá todas las funciones de lectura         //
+// de sensores, encoders y botones que se usarán en el programa. //
+///////////////////////////////////////////////////////////////////
+
+/**
+ * Función a ejecutar con cada tick del Canal A del motor derecho
+ */
+void tick_encoder_derecho_A(){
+
+}
+
+/**
+ * Función a ejecutar con cada tick del Canal B del motor derecho
+ */
+void tick_encoder_derecho_B(){
+
+}
+
+/**
+ * Función a ejecutar con cada tick del Canal A del motor izquierdo
+ */
+void tick_encoder_izquierdo_A(){
+
+}
+
+/**
+ * Función a ejecutar con cada tick del Canal B del motor izquierdo
+ */
+void tick_encoder_izquierdo_B(){
+
+}
