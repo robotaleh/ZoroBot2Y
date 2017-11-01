fcb2595eeb95dac5a798f3f1718c03ca0675c321
robotaleh <alex.santos.ete@gmail.com>
2017-11-01 16:09:31 +0100

    Añade funcion para contar las posibles salidas de cada celda

--- src/Algoritmo.ino
+++ src/Algoritmo.ino
@@ -15,3 +15,19 @@ void actualiza_casilla_actual(){
   casilla_actual[FRONTAL]       = valores_sensores_frontales[0] < umbral_deteccion_frontal || valores_sensores_frontales[1] < umbral_deteccion_frontal;
   casilla_actual[DERECHA]       = valores_sensores_laterales[1] > umbral_deteccion_lateral;
 }
+
+/**
+ * Actualiza los indicadores de pared de la celda actual y
+ * cuenta las posibles salidas que existen. Cuantas "no paredes" hay.
+ * @return Numero de salidas de la casilla (paredes faltantes)
+ */
+byte contar_salidas(){
+  byte salidas = 0;
+  actualiza_casilla_actual();
+  for(byte pared = 0; pared<3; pared++){
+    if(!casilla_actual[pared]){
+      salidas++;
+    }
+  }
+  return salidas;
+}
