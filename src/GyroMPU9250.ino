f14e5ac5762b33a689f9787f7a89f2f60118ef03
robotaleh <alex.santos.ete@gmail.com>
2017-11-05 12:38:40 +0100

    Añade variables y constantes iniciales para MPU9250

--- src/GyroMPU9250.ino
+++ src/GyroMPU9250.ino
@@ -2,3 +2,23 @@
 // Este fichero contendrá todas las funciones de control y   //
 // calibración del giroscopio MPU9250 integrado en el robot. //
 ///////////////////////////////////////////////////////////////
+#include <Wire.h>
+
+#define    MPU9250_ADDRESS            0x68
+#define    MAG_ADDRESS                0x0C
+
+#define    GYRO_FULL_SCALE_250_DPS    0x00
+#define    GYRO_FULL_SCALE_500_DPS    0x08
+#define    GYRO_FULL_SCALE_1000_DPS   0x10
+#define    GYRO_FULL_SCALE_2000_DPS   0x18
+
+#define    ACC_FULL_SCALE_2_G        0x00
+#define    ACC_FULL_SCALE_4_G        0x08
+#define    ACC_FULL_SCALE_8_G        0x10
+#define    ACC_FULL_SCALE_16_G       0x18
+
+int deadband_z = 0;
+float angle = 0;
+long last_angle_micros = 0;
+
+float kp_gyro = 1;
